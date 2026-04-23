bc19364edc159283a55563ba6dce338007b7a2b3
ArizonaChandler <tonynagga@gmail.com>
2026-04-24 00:21:49 +0300

    fix: авто-деплой

--- tier/__init__.py
+++ tier/__init__.py
@@ -1,14 +1,15 @@
 """Система повышения уровня TIER"""
 from tier.manager import tier_manager
-from tier.views import TierInfoView, TierModerationView
+from tier.views import TierSubmitView, TierModerationView, update_tier_embed
 from tier.settings_view import TierSettingsView
 from tier.initializer import setup as setup_tier
 from tier.base import PermanentView
 
 __all__ = [
     'tier_manager',
-    'TierInfoView',
+    'TierSubmitView',
     'TierModerationView',
+    'update_tier_embed',
     'TierSettingsView',
     'setup_tier',
     'PermanentView'
