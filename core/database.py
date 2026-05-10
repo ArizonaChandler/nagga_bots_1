bbcfe20d2753ac84b88fc5eb657e726e4e11ae4b
ArizonaChandler <tonynagga@gmail.com>
2026-05-11 01:40:46 +0300

    fix: авто-деплой

--- core/database.py
+++ core/database.py
@@ -419,8 +419,8 @@ class Database:
                         ('vacation_max_days', '30'))
 
             # ========== ВЫЗОВ ТАБЛИЦ СИСТЕМ ИГР ========== 
-            # self.init_games_tables()
-            print("⚠️ Таблицы игр временно отключены")
+            self.init_games_tables()
+            # print("⚠️ Таблицы игр временно отключены")
 
     # ===== СУЩЕСТВУЮЩИЕ МЕТОДЫ =====
     def add_user(self, discord_id: str, added_by: str):
