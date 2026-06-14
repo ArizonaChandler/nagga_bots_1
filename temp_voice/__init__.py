d15a4d2f4bd14ce5af75dfe46656ba12f5e8ef18
ArizonaChandler <tonynagga@gmail.com>
2026-06-14 06:14:27 +0300

    fix: авто-деплой

--- temp_voice/__init__.py
+++ temp_voice/__init__.py
@@ -1,7 +1,7 @@
 """Система временных голосовых комнат"""
 import logging
 from temp_voice.manager import temp_voice_manager
-from temp_voice.views import TempVoicePublicView, TempVoiceManageView
+from temp_voice.views import TempVoicePublicView
 from temp_voice.settings_view import TempVoiceSettingsView
 from temp_voice.initializer import setup as setup_temp_voice
 
@@ -11,7 +11,6 @@ logger.info("🎤 Модуль временных комнат загружен"
 __all__ = [
     'temp_voice_manager',
     'TempVoicePublicView',
-    'TempVoiceManageView',
     'TempVoiceSettingsView',
     'setup_temp_voice'
 ]
